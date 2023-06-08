d4d880fec9ad153048d17a370c4532082967896f
N.I <iworksWeb@gmail.com>
2023-06-08 11:03:47 +0900

    useStateで状態変数を定義し、猫画像をクリックで表示

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useState } from 'react'
 
 interface SearchCatImage {
   id: string;
@@ -13,6 +9,9 @@ interface SearchCatImage {
 }
 
 export default function Home() {
+  // 状態変数を定義
+  const [catImageUrl, setCatImageUrl] = useState("");
+
   const fetchCatImage = async (): Promise<SearchCatImage> => {
     const res = await fetch("https://api.thecatapi.com/v1/images/search");
     const result = await res.json();
@@ -30,13 +29,14 @@ export default function Home() {
 
   const handleClick = async () => {
     const catImage = await fetchCatImage();
+    setCatImageUrl(catImage.url)
   };
 
 
   return (
     <div className={styles.container}>
       <h1>猫画像アプリ</h1>
-      <img src="https://cdn2.thecatapi.com/images/2k4.jpg" alt="猫さん" />
+      <img src={catImageUrl} alt="猫さん" />
       <button onClick={handleClick}>今日の猫さん</button>
     </div>
   );
