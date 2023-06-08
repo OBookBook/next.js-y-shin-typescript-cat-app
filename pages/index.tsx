f4c1fcbc193764decec60371f10e38b1005c3a80
N.I <iworksWeb@gmail.com>
2023-06-08 11:31:34 +0900

    初期表示時、猫画像が読み取れるようにSSR(サーバサイドレンダリング)  実装 useStateに初期値を設定

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,41 @@
 import styles from '@/styles/Home.module.css'
+import { GetServerSideProps } from 'next';
 import { useState } from 'react'
 
 interface SearchCatImage {
   id: string;
   url: string;
   width: number;
-  heidht: number;
+  height: number;
 }
 
-export default function Home() {
-  // 状態変数を定義
-  const [catImageUrl, setCatImageUrl] = useState("");
+interface IndexPageProps {
+  initialCatImageUrl: string;
+}
 
-  const fetchCatImage = async (): Promise<SearchCatImage> => {
-    const res = await fetch("https://api.thecatapi.com/v1/images/search");
-    const result = await res.json();
-    // console.log(result);
-    /*
-      {
-        "id": "5r7",
-        "url": "https://cdn2.thecatapi.com/images/5r7.jpg",
-        "width": 600,
-        "height": 451
-      }
-    */
-    return result[0];
-  };
+const fetchCatImage = async (): Promise<SearchCatImage> => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const result = await res.json();
+  // console.log(result);
+  /*
+    {
+      "id": "5r7",
+      "url": "https://cdn2.thecatapi.com/images/5r7.jpg",
+      "width": 600,
+      "height": 451
+    }
+  */
+  return result[0];
+};
+
+export default function Home({ initialCatImageUrl }: IndexPageProps) {
+  // 状態変数を定義
+  const [catImageUrl, setCatImageUrl] = useState(initialCatImageUrl);
 
   const handleClick = async () => {
     const catImage = await fetchCatImage();
     setCatImageUrl(catImage.url)
   };
-
 
   return (
     <div className={styles.container}>
@@ -40,4 +44,14 @@ export default function Home() {
       <button onClick={handleClick}>今日の猫さん</button>
     </div>
   );
+};
+
+// SSR(サーバサイドレンダリング) 初期表示でも、猫の画像が見えるように
+export const getServerSideProps: GetServerSideProps<IndexPageProps> = async () => {
+  const catImage = await fetchCatImage();
+  return {
+    props: {
+      initialCatImageUrl: catImage.url,
+    },
+  };
 };
